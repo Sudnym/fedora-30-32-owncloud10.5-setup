bac1e63f4edfbac1d2455202471c649c58be00cd
Sudnym <mathescole@gmail.com>
2020-06-11 00:03:53 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Installing criu $(yes | dnf install criu) criu installed\ninstalling dependencies &(yes | dnf install container-selinux containerd libbsd libnet runc) dependencies installed.\nInstalling moby-engine.$(yes | dnf install moby-engine)moby-engine installed."
+echo "Installing dependencies $(yes | dnf install criu) $(yes | dnf install container-selinux containerd libbsd libnet runc) $(yes | dnf install moby-engine)"
 
-version = $(cat /etc/fedora-release | grep "31")
+version=$(cat /etc/fedora-release | grep "31")
 if [ $version = 31 ]
 then
 	echo "Enabling backwards compatibility for cgroups\n$(grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0")"
